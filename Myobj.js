cdd8080aac6e37c66e803489a8d082c9a6a8b22f
Jianglichi <jianglichi@hotmail.com>
2019-06-26 14:30:13 +0800

    first commit

---
+++ Myobj.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View,Image,StyleSheet,TextInput,Button,Alert,ScrollView,FlatList,SectionList,TouchableOpacity  } from 'react-native';
+
+
+
+class Myobj extends Component {
+  render() {
+    return <Button color="red" title="test" />;
+  }
+}
+
+export default Myobj;
---
+++ testobj.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View,Image,StyleSheet,TextInput,Button,Alert,ScrollView,FlatList,SectionList,TouchableOpacity  } from 'react-native';
+
+
+
+class Testobj extends Component {
+  render() {
+    return <Button color="red" title="TestobjTestobjTestobj" />;
+  }
+}
+
+export default Testobj;
